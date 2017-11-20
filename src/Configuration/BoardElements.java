a8c7dc6e391caddbd5bb421a4aab18dc9b7c5474
dmankows <mankowski.dominik@gmail.com>
2017-11-21 00:25:09 +0100

    update

--- src/Configuration/BoardElements.java
+++ src/Configuration/BoardElements.java
@@ -1,5 +1,8 @@
 package Configuration;
 
+/**
+ * Enum that defines whether the element in the array is empty, it is block, highlighted block or a character
+ */
 public enum BoardElements {
-    EMPTY, BLOCK, CHARACTER
+    EMPTY, BLOCK, HIGHLIGHTED, CHARACTER
 }
